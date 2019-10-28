fd751803aced9b4c5c5dbe32187c2a48b2c28585
Ojas <chimaneojas@gmail.com>
2019-10-28 20:54:31 +1100

    Initial Commit

---
+++ IOTAssignment3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOTAssignment3
+//
+//  Created by Ojas Chimane on 28/10/19.
+//  Copyright © 2019 Ojas Chimane Org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
